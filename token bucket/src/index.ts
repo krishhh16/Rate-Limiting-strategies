ad398b56292097add05bda2266d5da08844ba4a7
Zeref <krishh16@yahoo.com>
2024-05-11 10:43:56 +0530

    added some more stuff

--- token bucket/src/index.ts
+++ token bucket/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 
+// Initializing the app
 const app = express();
+
+const token = 5 // This variable specifies how many users can access a 
 
 app.get("", (req, res) => {
   console.log("hello world")
